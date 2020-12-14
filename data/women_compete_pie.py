43d009afaa3a8c861118ee5aa94c0479d024fd1c
evica-ai <e_mai@fanshaweonline.ca>
2020-12-14 17:11:04 -0500

    Updating html file and data files

--- data/women_compete_pie.py
+++ data/women_compete_pie.py
@@ -7,6 +7,9 @@ colors = ['light gold', 'silver', 'light brown']
 labels = ["Gold", "Silver", "Bronze"]
 
 explode = [0.1, 0.1, 0.1]
-plt.pie(values, labels=labels, colors=colors, explode=explode)
+fig1, ax1 = plt.subplots()
+ax1.pie(values, labels=labels, colors=colors, explode=explode, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 plt.show()
